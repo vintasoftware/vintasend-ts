db83ab9c3c0fbfa38032cf39c4505a24bb8dcbdf
Hugo bessa <hugo@vinta.com.br>
2025-03-05 02:07:57 -0300

    Adjust build

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { Notification } from "./types/notifications";
+export { Notification } from "./types/notification";
 export { NotificationService } from "./services/notification-service";
 export { NotificationContextRegistry } from "./services/notification-context-registry";
