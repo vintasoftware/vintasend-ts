122a16923ade189528b13fb3d8c29ee5ca7d287a
Hugo bessa <hugo@vinta.com.br>
2025-03-14 13:48:51 -0300

    Adjust default type exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
-export { Notification } from "./types/notification";
-export { VintaSendFactory } from "./services/notification-service";
-export type { VintaSend } from "./services/notification-service";
-export type { ContextGenerator } from "./types/notification-context-generators";
-export type { BaseNotificationTypeConfig } from "./types/notification-type-config";
-export type { BaseNotificationQueueService } from "./services/notification-queue-service/base-notification-queue-service";
-export type { BaseNotificationTemplateRenderer } from "./services/notification-template-renderers/base-notification-template-renderer";
-export type { BaseEmailTemplateRenderer } from "./services/notification-template-renderers/base-email-template-renderer";
+export { VintaSendFactory } from './services/notification-service';
+export type { VintaSend } from './services/notification-service';
+export type {
+  Notification,
+  DatabaseNotification,
+  NotificationInput,
+  NotificationResendWithContextInput,
+} from './types/notification';
+export type { ContextGenerator } from './types/notification-context-generators';
+export type { BaseNotificationTypeConfig } from './types/notification-type-config';
+export type { BaseNotificationQueueService } from './services/notification-queue-service/base-notification-queue-service';
+export type { BaseNotificationTemplateRenderer } from './services/notification-template-renderers/base-notification-template-renderer';
+export type { BaseEmailTemplateRenderer } from './services/notification-template-renderers/base-email-template-renderer';
